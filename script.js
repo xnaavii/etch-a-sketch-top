1cb150291be73d27788feeaaf374f33164b3ac88
Ivan Petrović <137888945+xnaavii@users.noreply.github.com>
2026-02-23 20:13:42 +0000

    Clean up comments

--- script.js
+++ script.js
@@ -1,15 +1,11 @@
-// Add a 16x16 Grid
-// Display a container
 // Grid container
 const container = document.createElement('div');
 container.classList.add('container');
 document.body.appendChild(container);
 
 const numberOfSquares = 16 * 16;
+// Create a 16x16 grid
 createSquares(container, numberOfSquares);
-
-// TODO: Add enough number of squares and
-// TODO: ensure it's 16x16 grid
 
 function createSquares(container, numOfSquares) {
   if (numOfSquares === 0) {
