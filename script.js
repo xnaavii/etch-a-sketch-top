759bc7ca3a449fc5205edd52c685a19fdbe94feb
Ivan Petrović <137888945+xnaavii@users.noreply.github.com>
2026-02-23 19:06:03 +0000

    Create a square

--- script.js
+++ script.js
@@ -1,4 +1,13 @@
+// Add a 16x16 Grid
+// TODO: Display a container
 // Grid container
 const container = document.createElement('div');
 container.classList.add('container');
 document.body.appendChild(container);
+
+// TODO: Create a square
+const square = document.createElement("div");
+// TODO: Add a square to the container
+// TODO: Add style to square
+// TODO: Add enough number of squares and
+// TODO: ensure it's 16x16 grid
